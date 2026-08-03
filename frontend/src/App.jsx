0dfbdc7b433a16ec92d93d414fd5133cc004b591
mitulgadhiya3706 <mitulgadhiya23@gmail.com>
2026-08-03 23:52:50 +0530

    Configure app routing and protected routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
+
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import CreateInvoice from "./pages/Invoices/CreateInvoice";
+import AllInvoices from "./pages/Invoices/AllInvoices";
+import InvoiceDetail from "./pages/Invoices/InvoiceDetail";
+import AIParser from "./pages/AI/AIParser";
+import ProfilePage from "./pages/Profile/ProfilePage";
+import ProtectedRoute from "./components/auth/ProtectedRoutes";
+
+
+function App() {
+  return (
+    <AuthProvider>
+      <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/" element={<ProtectedRoute />} >
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="invoices" element={<AllInvoices />} />
+              <Route path="invoices/new" element={<CreateInvoice />} />
+              <Route path="invoices/:id" element={<InvoiceDetail />} />
+              <Route path="workspace" element={<AIParser />} />
+              <Route path="profile" element={<ProfilePage />} />
+            </Route>
+
+              <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Router> 
+        <Toaster position="top-right" reverseOrder={false} />
+      </div>
+    </AuthProvider>
+  )
+}
+
+export default App;
